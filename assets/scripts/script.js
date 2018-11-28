bcf0a41d50d686feb51f7803f09c74f15be797eb
Anny Yang <annyyang@berkeley.edu>
2018-11-28 15:55:25 -0800

    added files

---
+++ assets/scripts/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function()  {
+	 $('.hidden').fadeIn(1000).removeClass('hidden');
+	/* $(window).load(function() {
+  	$("body").removeClass(".preload");
+	});*/
+
+	/* $(".navbar-item").click(function(){
+	 	$('.content').fadeOut(1000).addClass('hidden');
+
+	 })*/
+
+/*	 $(function(){
+  	$(".navbar-item").bind("click",function(){
+    $(this).animate({ $('.hidden').fadeOut(1000);
+	}, 100, function(){
+    window.location.href = $(this).attr("href");
+  })
+ })
+})*/
+
+
+});
